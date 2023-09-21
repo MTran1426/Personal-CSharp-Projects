589276bfbd7b987cebe140ddfd471cb246fb27ed
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2023-09-21 14:23:30 -0600

    went over variables and data types while testing how to apply different inputs in Console.WriteLine(). Added link to w3schools tutorial.

--- ProjectOne/ProjectOne/Program.cs
+++ ProjectOne/ProjectOne/Program.cs
@@ -6,7 +6,24 @@ namespace ProjectOne
     {
         static void Main(string[] args)
         {
+            string word = "Some word";
+            int integerNumber = 1;
+            double doubleNumber = 1.00;
+            char character = 'A';
+            bool boolean = true;
+            string maleName, femaleName;
+            maleName = "Jack";
+            femaleName = "Jill";
             Console.WriteLine("Hello World!");
+            Console.WriteLine(word);
+            Console.WriteLine(integerNumber);
+            Console.WriteLine(doubleNumber);
+            Console.WriteLine(character);
+            Console.WriteLine(boolean);
+            Console.WriteLine(integerNumber + doubleNumber);
+            Console.WriteLine(word + integerNumber + doubleNumber + character + boolean);
+            Console.WriteLine(maleName + " and " + femaleName + " like spending time together.");
+
         }
     }
 }
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This repository will be used for projects that are focused on C# projects.
 
 | Order of Projects | Description |
 | --- | --- |
-|        1.         | Create a basic Hello World project that reviews the beginner aspects of C#. |
+|        1.         | Create a basic Hello World project that reviews the beginner aspects of C#. Following the contents that are on https://www.w3schools.com/cs/cs_data_types.php |
 |        2.         |  |
 |        3.         |  |
 |        4.         |  |
