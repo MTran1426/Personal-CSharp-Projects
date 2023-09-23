cc969415e0fadd2092e5b892ed7ad6a85428b7db
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2023-09-22 19:39:12 -0600

    Created projectTwo and started work on console inputs.

---
+++ ProjectTwo/ProjectTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjectTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opening Prompt
+            Console.WriteLine("Please enter your username: ");
+            //Use Console.ReadLine to read the inputted username
+            string userName = Console.ReadLine();
+            //Output will show a string and the username
+            Console.WriteLine("The username is: " + userName);
+        }
+    }
+}
