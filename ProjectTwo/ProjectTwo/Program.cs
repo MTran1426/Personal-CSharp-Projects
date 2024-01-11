3c949657e8628a3be0860315832648ad9df20ca6
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2024-01-11 15:34:48 -0700

    Finished work up to Comparison Operators

--- ProjectTwo/ProjectTwo/Program.cs
+++ ProjectTwo/ProjectTwo/Program.cs
@@ -21,16 +21,40 @@ namespace ProjectTwo
             //Console.WriteLine("User age is " + userAge);
 
             //Basic Addition Input
-            Console.WriteLine("Please input two numbers to be added together.");
-            Console.WriteLine("First number: ");
-            string firstInput = Console.ReadLine();
-            Console.WriteLine("Second number: ");
-            string secondInput = Console.ReadLine();
-            int firstNumber = int.Parse(firstInput);
-            int secondNumber = int.Parse(secondInput);
-            int totalNumber = firstNumber + secondNumber;
-            Console.WriteLine("The total of " + firstNumber + " + " + secondNumber + " is " + totalNumber);
-            
+            //Console.WriteLine("Please input two numbers to be added together.");
+            //Console.WriteLine("First number: ");
+            //string firstInput = Console.ReadLine();
+            //Console.WriteLine("Second number: ");
+            //string secondInput = Console.ReadLine();
+            //int firstNumber = int.Parse(firstInput);
+            //int secondNumber = int.Parse(secondInput);
+            //int totalNumber = firstNumber + secondNumber;
+            //Console.WriteLine("The total of " + firstNumber + " + " + secondNumber + " is " + totalNumber);
+
+            //Assignment Operator
+            //int addition = 69;
+            //addition += 1;
+
+            //int subtraction = 69;
+            //subtraction -= 1;
+
+            //int division = 69;
+            //division /= 1;
+
+            //int multiplication = 69;
+            //multiplication *= 1;
+
+            //double modulus = 69;
+            //modulus %= 3;
+
+            //Console.WriteLine("Addition = " + addition);
+            //Console.WriteLine("Subtraction = " + subtraction);
+            //Console.WriteLine("Division = " + division);
+            //Console.WriteLine("Multiplication = " + multiplication);
+            //Console.WriteLine("Modulus = " + modulus);
+
+            //Comparison Operators
+
         }
     }
 }
