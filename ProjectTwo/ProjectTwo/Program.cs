634618291bc97a34a9be686badb7e5b61702bd7d
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2024-01-18 17:07:03 -0700

    Finished up to Booleans

--- ProjectTwo/ProjectTwo/Program.cs
+++ ProjectTwo/ProjectTwo/Program.cs
@@ -54,7 +54,143 @@ namespace ProjectTwo
             //Console.WriteLine("Modulus = " + modulus);
 
             //Comparison Operators
+            //Console.WriteLine("Please input a value for x : ");
+            //string input = Console.ReadLine();
+            //int x = int.Parse(input);
+            //bool answer = false;
+            //if (x == 1)
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x = 1 is : " + answer);
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("x = 1 is : " + answer);
+            //}
 
-        }
-    }
-}
+            //if (x != 1)
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x != 1 is : " + answer);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("x != 1 is : " + answer);
+            //}
+
+            //if (x > 1)
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x > 1 is : " + answer);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("x > 1 is : " + answer);
+            //}
+
+            //if (x < 1)
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x < 1 is : " + answer);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("x < 1 is : " + answer);
+            //}
+
+            //if (x >= 1)
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x >= 1 is : " + answer);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("x >= 1 is : " + answer);
+            //}
+
+            //if (x <= 1)
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x <= 1 is : " + answer);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("x <= 1 is : " + answer);
+            //}
+
+            //Logical Operators
+            //Console.WriteLine("Please input a value for x : ");
+            //string input = Console.ReadLine();
+            //Console.WriteLine("Please input a value for y : ");
+            //string secondInput = Console.ReadLine();
+            //int x = int.Parse(input);
+            //int y = int.Parse(secondInput);
+            //bool answer = false;
+            //if (x == 1 && y == 2)
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x = 1 and y = 2 is : " + answer);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("x = 1 and y = 2 is : " + answer);
+            //}
+
+            //if (x == 1 || y == 2)
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x = 1 or y = 2 is : " + answer);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("x = 1 or y = 2 is : " + answer);
+            //}
+
+            //if (!(x == 1 && y == 2))
+            //{
+            //    answer = false;
+            //    Console.WriteLine("x != 1 and y != 2 is : " + answer);
+            //}
+            //else
+            //{
+            //    answer = true;
+            //    Console.WriteLine("x != 1 and y != 2 is : " + answer);
+            //}
+
+            //Math
+            //Console.WriteLine("Input the value of x : ");
+            //string input = Console.ReadLine();
+            //Console.WriteLine("Input the value of y :");
+            //string secondInput = Console.ReadLine();
+            //int x = int.Parse(input);
+            //int y = int.Parse(secondInput);
+            //Console.WriteLine("The maximum value between " + x + " and " + y + " is " + Math.Max(x, y));
+            //Console.WriteLine("The minimum value between " + x + " and " + y + " is " + Math.Min(x, y));
+            //Console.WriteLine("The square root of " + x + " is " + Math.Sqrt(x));
+            //Console.WriteLine("The absolute value of " + x + " is " + Math.Abs(x));
+            //Console.WriteLine("The rounded value of " + x + " is " + Math.Round(x));
+
+            //Strings
+            //Console.WriteLine("Please input a sentence: ");
+            //string input = Console.ReadLine();
+            //Console.WriteLine("Your sentence is " + input.Length + " characters long."); //outputs the length of the string
+            //Console.WriteLine(input.ToUpper()); //outputs all upper case
+            //Console.WriteLine(input.ToLower()); //outputs all lower case
+
+            //String Interpolation
+            //string firstName = "John";
+            //string lastName = "Doe";
+            //string name = $"My full name is: {firstName} {lastName}";
+            //Console.WriteLine(name);
+
+            //Access Strings
+            //string word = "Yeet";
+            //Console.WriteLine("The first letter of " + word + " is " + word[0]);
+            //Console.WriteLine("The second letter of " + word + " is " + word[1]);
+            //Console.WriteLine("The index of the letter t is " + word.IndexOf("t"));
+
+
+
+        }//void main
+    }//class program
+}//namespace
