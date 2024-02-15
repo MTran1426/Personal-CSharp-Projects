49bb641c1b08c8d5dab0d7d1577db4ebcecc3f21
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2024-02-14 17:33:26 -0700

    Created ProjectThree and done up to the Return Values section.

---
+++ ProjectThree/ProjectThree/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ProjectThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test method
+            //testMethod();
+
+            //parameter method
+            //int inputOne,
+            //    inputTwo;
+
+            //Console.WriteLine("Please input the first number for the addition statement: ");
+            //string numberOne = Console.ReadLine();
+            //Console.WriteLine("Please input the second number for the addition statement: ");
+            //string numberTwo = Console.ReadLine();
+
+            //inputOne = int.Parse(numberOne);
+            //inputTwo = int.Parse(numberTwo);
+
+            //parameterMethod(inputOne, inputTwo);
+
+            //default parameter method
+            //defaultParameterMethod();
+            //defaultParameterMethod("Doe"); //Input a string to change the value of the default set by parameter
+
+        }//void main
+
+        //Basic Method
+        //static void testMethod() 
+        //{
+        //    Console.WriteLine("This is a test method call.");
+        //}
+
+        //Method with Parameters
+        //static void parameterMethod(int firstInput, int secondInput)
+        //{
+        //    Console.WriteLine(firstInput + " + " + secondInput + " = " + (firstInput + secondInput));
+        //}
+
+        //Method with default parameter
+        //static void defaultParameterMethod(string myName = "John") 
+        //{
+        //    Console.WriteLine("My name is " + myName);
+        //}
+
+    }//class program
+}//namespace ProjectThree
