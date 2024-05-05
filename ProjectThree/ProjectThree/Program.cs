8c2c7ccaa571efa9b175ecd41268d4e0fe803418
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2024-05-04 21:09:52 -0600

    Finished Classes with Methods.

--- ProjectThree/ProjectThree/Program.cs
+++ ProjectThree/ProjectThree/Program.cs
@@ -56,7 +56,8 @@ namespace ProjectThree
             //    Console.WriteLine(obsidianChestItem);
             //}
 
-
+            SortingChest sortingChest = new SortingChest();
+            sortingChest.dataOutput(sortingChest.importantItems, sortingChest.itemAmount);
 
         }//void main
 
@@ -104,39 +105,7 @@ namespace ProjectThree
 
     //}//class Inventory
 
-    //Creating a class with fields and methods
-    public class SortingChest
-    {
-        string[] importantItems;
-        int[] itemAmount;
-
-        public void dataOutput(string[] importantItems, int[] itemAmount)
-        {
-            foreach (string item in importantItems)
-            {
-                Console.WriteLine(item);
-            }
-
-            foreach (int value in itemAmount)
-            {
-                Console.WriteLine(value);
-            }
-
-        }//method
-
-        public void Main(string[] args)
-        {
-            SortingChest sortingChest = new SortingChest();
-            string[] itemArray = { "Diamond", "Iron Ingot", "Lapis Lazuli" };
-            sortingChest.importantItems = itemArray;
-            int[] valueArray = { 5, 20, 7 };
-            sortingChest.itemAmount = valueArray;
-
-            sortingChest.dataOutput(itemArray, valueArray);
-           
-        }
-
-    }//class SortingChest
+    
 
 
 
---
+++ ProjectThree/SortingChest/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SortingChest
+{
+    public class Class1
+    {
+    }
+}
