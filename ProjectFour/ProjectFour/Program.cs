1c456c6ef1cffba278eede33be40831a8f7fffc3
Melvin Tran <77076819+MTran1426@users.noreply.github.com>
2024-06-10 15:43:32 -0600

    Created ProjectFour and gitignore file

---
+++ ProjectFour/ProjectFour/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Media;
+
+namespace ProjectFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Playing music");
+            PlayMusic("C:\\Github\\Personal-CSharp-Projects\\ProjectFour\\ProjectFour\\ThenRise.wav");
+        }
+
+        public static void PlayMusic(string filePath)
+        {
+            SoundPlayer musicPlayer = new SoundPlayer();
+            musicPlayer.SoundLocation = filePath;
+            musicPlayer.PlaySync();
+        }
+    }
+}
